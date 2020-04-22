435dad6dcf1fa53afc64c8e6528bd9c4bbdef9b7
enkhtuvshin <enkhhe.yahoo@gmail.com>
2020-04-22 23:24:59 +0800

    projectd  baih huwisagchdiig zarlaj ogson

---
+++ app.js
@@ -0,0 +1,14 @@
+//toglogchiin eeljiig solih heseg
+var activePlayer = 1;
+
+//тоглогчийн цуглуулсан оноог хадгаллдаг хувьсагч
+var scores = [0, 0];
+//eeljind tsugluulj bui onoog hadgaldag huwisagch
+var roundScore = 0;
+//shoonii taliig hadgalah huwisagch
+//<div class="player-score" id="score-0">43</div>
+
+var dice = Math.floor(Math.random() * 6) + 1;
+// document.querySelector("#score-0").textContent = "<em>sss</em>";
+
+console.log(dice);
