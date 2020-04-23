a8c544162b08a417014c41f025f848e6d2dbdbed
enkhtuvshin <enkhhe.yahoo@gmail.com>
2020-04-23 12:00:49 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,7 +8,20 @@ var roundScore = 0;
 //shoonii taliig hadgalah huwisagch
 //<div class="player-score" id="score-0">43</div>
 
-var dice = Math.floor(Math.random() * 6) + 1;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
+
 // document.querySelector("#score-0").textContent = "<em>sss</em>";
 
 console.log(dice);
